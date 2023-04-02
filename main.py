8fb930beaa79f6ca7fc81710b6256b84f6096098
Олег <mishin415@yandex.ru>
2023-04-02 23:31:41 +0300

    parsing

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-number = int(input())
-times = int(input())
+from bs4 import BeautifulSoup
+import requests
 
-for i in range(times):
-    number *= 2
-print(number)
+url = "https://ru.wikipedia.org/wiki/Приозерск"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'html.parser')
+paragraphs = []
+remaining_content = []
+remaining_content = soup.find_all()
+for tag in remaining_content:
+    if tag.find("p") is not None:
+        paragraphs.append(tag.text)
+for tag in paragraphs:
+
+    print(tag)
